d25225df0724babf68f1a8594b791105979db083
shifan <lirpt98@gmail.com>
2024-09-06 16:53:25 +0800

    update

--- Runtime/Event/GameEventArgs.cs
+++ Runtime/Event/GameEventArgs.cs
@@ -1,10 +1,24 @@
+using System;
+
 namespace SimpleFramework
 {
     /// <summary>
     /// 游戏逻辑事件基类。
     /// </summary>
-    public abstract class GameEventArgs : BaseEventArgs
+    public abstract class GameEventArgs : EventArgs, IReference
     {
-		
+        /// <summary>
+        /// 获取类型编号
+        /// </summary>
+        public abstract int Id
+        {
+            get;
+        }
+
+        /// <summary>
+        /// 清理引用。
+        /// </summary>
+        public abstract void Clear();
     }
 }
+
